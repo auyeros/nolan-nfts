bd251d73de9f515a658728af1b611085226a6a47
tomas oliver <tomi204@outlook.com>
2022-10-05 21:01:04 -0300

    agregados mejor postor requires y viewBalance

--- truffle/contracts/MarketPlace.sol
+++ truffle/contracts/MarketPlace.sol
@@ -3,15 +3,19 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract MarketPlace{
 
- address payable public owner; 
+ address payable public owner;
+ //high offer 
+ address highestBidder;
+ uint highestOffer;
 
+ //emum
  enum State {Active, Inactive}  
 
  State public state = State.Inactive;
 
 mapping(address => uint) biddersData;
 
-uint calculateAmount = biddersData[msg.sender] + msg.value;
+
 
 constructor(){
 
@@ -22,21 +26,40 @@ constructor(){
        require(owner == msg.sender, "you need to be the project owner");
        _;
    }
-   
-   function changeOperator(State newSate) public Onlyowner{
+
+   //changeOperator()
+function changeOperator(State newSate) public Onlyowner{
        state = newSate;
    }
-function putBid() public payable{
-    //verify value is not zero 
-    require (state == State.Active);
-     require(msg.value >0, "bid amount cannot be 0");
-    biddersData[msg.sender] = msg.value;
-}
 
-function getBidderBid(address _address) public view returns(uint){
+   //placeOffering()
+function placeOfferring() public payable{
+    //verify value is not zero 
+    uint calculateAmount = biddersData[msg.sender] + msg.value;
+
+    require (state == State.Active);
+    require(msg.value >0, "bid amount cannot be 0");
+    require(calculateAmount >highestOffer, "Highest bid alredy present");
+
+    biddersData[msg.sender] = calculateAmount;
+    highestOffer = calculateAmount;
+    highestBidder=msg.sender;
+}
+// highest offer
+function highestBid()public view returns(uint){
+    return highestOffer;
+}
+// highest offer address
+function addressFromHB()public view returns(address){
+    return highestBidder;
+}
+//viewBalance
+function viewBalance(address _address) public view returns(uint){
 return biddersData[_address];
 
 }
+
+//withdrawBalance from the dapp
 function withdrawBalance()public payable Onlyowner{
         owner.transfer(address(this).balance);
     }
