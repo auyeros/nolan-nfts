d817d39ecd364a5cfd6786cf802a222d802839a3
tomi204 <tomi204@outlook.com>
2022-10-21 18:07:29 -0300

    avanzando con el contrato unico

--- contracts/Combo.sol
+++ contracts/Combo.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.17;
+pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Marketplace{
-    //state 
+  IERC20 s_token; 
   IERC721 s_NFTs;
-
+ //state 
   address  payable  isOwner;
 
 // state in The sale
@@ -15,12 +16,6 @@ contract Marketplace{
         Active,
         Cancelled
     }
- //state of contract 
-    enum StateOfAuction {Active, Inactive, Canceled}  
-
-
-  StateOfAuction public stateA = StateOfAuction.Inactive;
-
 
   struct Sale {
       address owner;
@@ -30,20 +25,37 @@ contract Marketplace{
         
 //mappings        
   mapping(uint256 => Sale) public s_sales;
-  mapping(uint256 => uint256) s_refNFTs;
   mapping(uint256 => uint256) s_security;
 
+////////////////////////////////////////////////////////////////////////
+  // auction 
+// state of the auction 
+ enum StateOfAuction {Active, Inactive, Canceled}  
+
+
+  StateOfAuction public stateA = StateOfAuction.Inactive;
+
+  // struc for the nft in the auction
+  struct SaleAuction {
+      address owner;
+      uint256 price;
+      StateOfAuction status;
+  }
+  uint public highestBid;
+  address public highestBidder;
+  mapping(address => uint) biddersData;
+  mapping(uint256 => SaleAuction) public s_salesA;
 //counters
   using Counters for Counters.Counter;
   Counters.Counter s_counter;
 
-//modifier
-//only owner
+                     //modifiers
+               //onlyowner
   modifier onlyOwner{
       isOwner == payable(msg.sender);
       _;
   }
-//securityFrontRunning security
+              //securityFrontRunning security
   modifier securityFrontRunning(uint256 p_nftID){
       require(
           s_security[p_nftID] == 0 || s_security[p_nftID] > block.number,
@@ -53,10 +65,14 @@ contract Marketplace{
           _;         
   }
        //constructor
-       constructor (address p_NftsContract){
-           s_NFTs = IERC721(p_NftsContract);
-       }
+       constructor (address p_stableCoinUsdContract, address p_nftsContract) {
+         isOwner = payable(msg.sender);
+        s_token = IERC20(p_stableCoinUsdContract);
+        s_NFTs = IERC721(p_nftsContract);
+    }
 
+
+// open sale(for sale one nft)
     function openSale(uint256 p_nftID, uint256 p_price) public securityFrontRunning(p_nftID) {
         if (s_sales[p_nftID].owner == address(0)) {
             s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
@@ -77,20 +93,24 @@ contract Marketplace{
             s_sales[p_nftID].status = Status.Active;
             s_sales[p_nftID].price = p_price;
         }
-    }
+  }
        
+// cancel sale(for cancel one sale of nft)
+   function cancelSale(uint256 p_nftID) public securityFrontRunning(p_nftID) {
+        require(
+            msg.sender == s_sales[p_nftID].owner,
+            "Without permission"
+        );
 
-       function cancelSale(uint p_nftID) public securityFrontRunning(p_nftID) onlyOwner{
-         uint256 pos = s_refNFTs[p_nftID];
+        require(s_sales[p_nftID].status == Status.Active, "Is not Open");
 
-         require(
-           msg.sender == s_sales[pos].owner, "you need be a owner");
-         require(s_sales[pos].status == Status.Active);
-         s_sales[pos].status = Status.Cancelled;
-         s_NFTs.transferFrom(address(this), s_sales[pos].owner, p_nftID);
-       }
+        s_sales[p_nftID].status = Status.Cancelled;
 
-         function buy(uint256 p_nftID, uint256 p_price) public  securityFrontRunning(p_nftID) {
+        s_NFTs.transferFrom(address(this), s_sales[p_nftID].owner, p_nftID);
+    }
+
+//buy(for buy direct one nft)
+     function buy(uint256 p_nftID, uint256 p_price) public  securityFrontRunning(p_nftID) {
         require(s_sales[p_nftID].status == Status.Active, "Is not Open");
 
         address oldOwner = s_sales[p_nftID].owner;
@@ -101,30 +121,56 @@ contract Marketplace{
         s_sales[p_nftID].owner = msg.sender;
         s_sales[p_nftID].status = Status.Inactive;
 
-       // require(s_token.transferFrom(msg.sender, oldOwner, price), "Error transfer token - price");
-       // require(s_token.transferFrom(msg.sender, address(this), (price / 100) * 3), "Error transfer fee"); // fee 3%
+       require(s_token.transferFrom(msg.sender, oldOwner, price), "Error transfer token - price");
+       require(s_token.transferFrom(msg.sender, address(this), (price / 100) * 3), "Error transfer fee"); // fee 3%
 
         s_NFTs.transferFrom(address(this), msg.sender, p_nftID);
     }
-     // withdrawBalance
-        function withdrawBalance() public onlyOwner{
+
+
+     // withdrawBalance from the dapp
+    function withdrawBalance() public onlyOwner{
      require(msg.sender == isOwner);
         isOwner.transfer(address(this).balance);
         emit balanceWithdrawn("balance"); // emit event 
     }
-
-      
+////////////////////////////////////////////////////////////////////////   
+////////////////////auction
+/////////////////////////////////////////
+  //change operator of the auction
   function changeOperator(StateOfAuction newSate) public onlyOwner{
        stateA = newSate; 
        emit OperatorChanged("auction closed");// emit event
    }
 
+   function startAuction(uint256 p_nftID, uint256 p_price) public securityFrontRunning(p_nftID) {
+        if (s_salesA[p_nftID].owner == address(0)) {
+            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
+
+            s_salesA[p_nftID] = SaleAuction(
+                msg.sender,
+                p_price,
+                StateOfAuction.Active
+            );
+        } else {
+            require(
+                msg.sender == s_salesA[p_nftID].owner,
+                "Without permission"
+            );
+
+            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
+
+            s_salesA[p_nftID].status = StateOfAuction.Active;
+            s_salesA[p_nftID].price = p_price;
+        }
+  }
+ 
      
     //events 
 
-event OperatorChanged(
-  string newSate
-);
+    event OperatorChanged(
+      string newSate
+   );
 
     event balanceWithdrawn(
       string balanceInApp
