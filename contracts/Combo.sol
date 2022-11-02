dce1d211d957032a15a460a4d436d989d9cbc5e3
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-11-02 16:13:39 -0300

    end at added

--- contracts/Combo.sol
+++ contracts/Combo.sol
@@ -1,227 +1,236 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.7;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Marketplace{
-  IERC20 s_token; 
-  IERC721 s_NFTs;
- //state 
-  address  payable  isOwner;
+contract MarketPlaceNFT is ReentrancyGuard {
+    //state variables
 
-// state in The sale
-  enum Status{
-        Inactive,
-        Active,
-        Cancelled
+    address payable public immutable feeAccount; // account receives fees
+    uint256 public immutable feePercent; //fee percentage
+    uint256 public itemCount;
+
+    struct Item {
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
     }
 
-  struct Sale {
-      address owner;
-      uint256 price;
-      Status status;
-  }
-        
-//mappings        
-  mapping(uint256 => Sale) public s_sales;
-  mapping(uint256 => uint256) s_security;
+    address[] public seller;
 
-////////////////////////////////////////////////////////////////////////
-  // auction 
-// state of the auction 
- enum StateOfAuction {Active, Inactive, Canceled}  
+    mapping(uint256 => Item) public items;
 
-uint highestOffer;
-  StateOfAuction public stateA = StateOfAuction.Inactive;
-
-  // struc for the nft in the auction
-  struct SaleAuction {
-      address owner;
-      uint256 price;
-      StateOfAuction status;
-  }
-  uint public highestBid;
-  address public highestBidder;
-  mapping(address => uint) biddersData;
-  mapping(uint256 => SaleAuction) public s_salesA;
-//counters
-  using Counters for Counters.Counter;
-  Counters.Counter s_counter;
-
-                     //modifiers
-               //onlyowner
-  modifier onlyOwner{
-      isOwner == payable(msg.sender);
-      _;
-  }
-              //securityFrontRunning security
-  modifier securityFrontRunning(uint256 p_nftID){
-      require(
-          s_security[p_nftID] == 0 || s_security[p_nftID] > block.number,
-           "error security");
-               
-          s_security[p_nftID] = block.number;
-          _;         
-  }
-       //constructor
-       constructor (address p_stableCoinUsdContract, address p_nftsContract) {
-         isOwner = payable(msg.sender);
-        s_token = IERC20(p_stableCoinUsdContract);
-        s_NFTs = IERC721(p_nftsContract);
+    constructor(uint256 _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
     }
 
-//////////////////////////////////////////////
-///////////////DIRECT BUY
-/////////////////////////////////////////////
+    modifier onlyOwner() {
+        require(msg.sender == feeAccount);
+        _;
+    }
 
-// open sale(for sale one nft)
-    function openSale(uint256 p_nftID, uint256 p_price) public securityFrontRunning(p_nftID) {
-        if (s_sales[p_nftID].owner == address(0)) {
-            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
+    //functions
+    ///sell NFT
+    function sellNFT(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        require(_price > 0, "price must be greater than zero");
+        itemCount++;
 
-            s_sales[p_nftID] = Sale(
-                msg.sender,
-                p_price,
-                Status.Active
-            );
-        } else {
-            require(
-                msg.sender == s_sales[p_nftID].owner,
-                "Without permission"
-            );
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
 
-            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
-
-            s_sales[p_nftID].status = Status.Active;
-            s_sales[p_nftID].price = p_price;
-        }
-  }
-       
-// cancel sale(for cancel one sale of nft)
-   function cancelSale(uint256 p_nftID) public securityFrontRunning(p_nftID) {
-        require(
-            msg.sender == s_sales[p_nftID].owner,
-            "Without permission"
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
         );
-
-        require(s_sales[p_nftID].status == Status.Active, "Is not Open");
-
-        s_sales[p_nftID].status = Status.Cancelled;
-
-        s_NFTs.transferFrom(address(this), s_sales[p_nftID].owner, p_nftID);
+        emit newNFT(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 
-//buy(for buy direct one nft)
-     function buy(uint256 p_nftID, uint256 p_price) public  securityFrontRunning(p_nftID) {
-        require(s_sales[p_nftID].status == Status.Active, "Is not Open");
+    //buy direct
+    function buyNFT(uint256 _itemId) external payable nonReentrant {
+        uint256 _totalPrice = getTotalPrice(_itemId); //getting total price
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId <= itemCount, "item doesnt exist"); //checking item
+        require(!item.sold, "item already sold"); /// checking item
+        require(
+            msg.value >= _totalPrice,
+            "not enough ether to cover item price and market fee"
+        ); //require anti scam
+        feeAccount.transfer(_totalPrice - item.price); //fee for nft marketplace
+        item.seller.transfer(item.price); // send value to seller
+        item.sold = true; // set sold = true
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId); // trasnfer the nft to the buyer
+        emit NftPurchased(
+            _itemId,
+            address(item.nft),
+            address(item.seller),
+            true,
+            msg.sender
+        ); //event nft purchased
+    }
 
-        address oldOwner = s_sales[p_nftID].owner;
-        uint256 price = s_sales[p_nftID].price;
+    function getTotalPrice(uint256 _itemId) public view returns (uint256) {
+        return ((items[_itemId].price * (100 + feePercent)) / 100); //calculate total price
+    }
+
+    function setSeller(address payable _seller) public onlyOwner {
+        seller.push(_seller); //push seller
+    }
+
+    function cancelSell(uint256 _itemId) external {
+        Item storage item = items[_itemId];
+        require(msg.sender == item.seller, "you dont are the owner of the nft");
+        require(item.sold != true);
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId); // trasnfer the nft to ex seller
+        item.sold = true;
+    }
+
+    ///events
+    event newNFT(
+        uint256 itemId,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 price,
+        address indexed seller
+    );
+    event NftPurchased(
+        uint256 itemId,
+        address indexed nft,
+        address indexed seller,
+        bool sold,
+        address indexed buyer
+    );
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// Auction code
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    //State of items
+    enum State {
+        Active,
+        Inactive,
+        Canceled
+    }
+
+    struct itemAuction {
+        // struct of itemAuction
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 startPrice;
+        address payable seller;
+        State state;
+        bool sold;
+        uint endAt;
+       // uint256 creatorFee;
+        address payable highestBidder; // best bidder address
+        uint256 highestBid; // best bid amount
+    }
+    uint256 public itemCountA; ////itemCount
+
+    mapping(uint256 => itemAuction) public itemsAuction; ///mapping de los items
+    mapping(address => uint256) public bids; //bids
+
+    //change operator
+    function changeOperator(uint256 _itemId, State _newState) public {
+        itemAuction storage ItemAuction = itemsAuction[_itemId];
+        _itemId = ItemAuction.itemId;
+        require(
+            msg.sender == ItemAuction.seller,
+            "you dont are the owner of the nft"
+        );
+        ItemAuction.state = _newState;
+    }
+
+    ///get total price for auction
+    function getTotalPriceAuction(uint256 _itemId)
+        public
+        view
+        returns (uint256)
+    {
+        return (itemsAuction[_itemId].highestBid / feePercent); //calculate total price
+    }
+
+    //start nft auction
+    function startAuction(
+        IERC721 _nftA,
+        uint256 _tokenId,
+        uint256 _startPrice,
+        uint256 _creatorFee
+    ) public {
+        require(_startPrice > 0, "price must be greater than zero");
+        itemCountA++;
+        _nftA.transferFrom(msg.sender, address(this), _tokenId);
+
         
-        require(price == p_price, "Manipulated price");
-
-        s_sales[p_nftID].owner = msg.sender;
-        s_sales[p_nftID].status = Status.Inactive;
-
-       require(s_token.transferFrom(msg.sender, oldOwner, price), "Error transfer token - price");
-       require(s_token.transferFrom(msg.sender, address(this), (price / 100) * 3), "Error transfer fee"); // fee 3%
-
-        s_NFTs.transferFrom(address(this), msg.sender, p_nftID);
+        itemsAuction[itemCountA] = itemAuction(
+            itemCountA,
+            _nftA,
+            _tokenId,
+            _startPrice,
+            payable(msg.sender),
+            State.Active,
+            false,
+            endAt = block.timestamp + 7 days;
+            _creatorFee,
+            payable(address(0)),
+            0
+        );
     }
 
-
-     // withdrawBalance from the dapp
-    function withdrawBalance() public onlyOwner{
-     require(msg.sender == isOwner);
-        isOwner.transfer(address(this).balance);
-        emit balanceWithdrawn("balance"); // emit event 
-    }
-////////////////////////////////////////////////////////////////////////   
-////////////////////auction
-/////////////////////////////////////////
-
-//////////////////////////////////////////
-  //change operator of the auction
-////////////////////////////////////////
-  function changeOperator(StateOfAuction newSate) public onlyOwner{
-       stateA = newSate; 
-       emit OperatorChanged("auction closed");// emit event
-   }
-///////////////
-// StartAuction
-//////////////
-   function startAuction(uint256 p_nftID, uint256 p_price) public securityFrontRunning(p_nftID) {
-        if (s_salesA[p_nftID].owner == address(0)) {
-            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
-
-            s_salesA[p_nftID] = SaleAuction(
-                msg.sender,
-                p_price,
-                StateOfAuction.Active
-            );
-        } else {
-            require(
-                msg.sender == s_salesA[p_nftID].owner,
-                "Without permission"
-            );
-
-            s_NFTs.transferFrom(msg.sender, address(this), p_nftID);
-
-            s_salesA[p_nftID].status = StateOfAuction.Active;
-            s_salesA[p_nftID].price = p_price;
+    function placeOffering(uint256 _itemId) public payable {
+        itemAuction storage itemA = itemsAuction[_itemId];
+        require(State.Active == itemA.state, "error state is inactive");
+        require(itemA.sold == false, "error item sold");
+        require(msg.value > itemA.startPrice, "error we need more ether");
+        require(msg.value > itemA.highestBid, "error you need send more ether");
+        require(itemA.endAt > 0 );
+        if (itemA.highestBidder != msg.sender) {
+            //security
+            itemA.highestBidder.transfer(itemA.highestBid); //trasnfer money for old best bidder
         }
-  }
+        itemA.highestBid = msg.value; // value sent
+        itemA.highestBidder = payable(msg.sender); //new best bidder address
+        emit Bid(itemA.highestBidder, itemA.highestBid); // event new best bidder
+    }
 
+    event Bid(address bidderAddress, uint256 bidderOffer); // event with best bidder
 
+    function closeOffering(uint256 _itemId) external payable {
+        uint256 _totalPrice = getTotalPriceAuction(_itemId);
+        itemAuction storage ItemAuction = itemsAuction[_itemId];
+        require(State.Active == ItemAuction.state, "error state is inactive");
+        require(
+            msg.sender == ItemAuction.seller,
+            "you dont are the owner of the nft"
+        );
+        require(endAt == 0);
+        require(ItemAuction.sold == false, "error nft sold");
+        ItemAuction.sold = true;
+        ItemAuction.state = State.Inactive;
+        feeAccount.transfer(_totalPrice); //fee for nft marketplace
+        ItemAuction.seller.transfer(ItemAuction.highestBid - _totalPrice); // send value to seller
+        emit End(ItemAuction.highestBidder, ItemAuction.highestBid);
+    }
 
+    function cancelAuction(uint256 _itemId) external {
+        itemAuction storage ItemAuction = itemsAuction[_itemId];
+        require(msg.sender == ItemAuction.seller, "you dont are the owner of the nft");
+        require(ItemAuction.sold != true);
+        ItemAuction.nft.transferFrom(address(this), msg.sender, ItemAuction.tokenId); // trasnfer the nft to ex seller
+        ItemAuction.state = State.Canceled;
+    }
 
-
-
-
-
-
-////////////////////////////
-////place offering
-///////////////////////////
-
-function placeOfferring(uint256 p_nftID) public payable{
-    //verify value is not zero
-    uint calculateAmount = biddersData[msg.sender] + msg.value;
-    require (s_salesA[p_nftID].status == StateOfAuction.Active);
-    require(msg.value >0, "bid amount cannot be 0");
-    require(calculateAmount >highestOffer, "Highest bid alredy present");  
-    biddersData[msg.sender] = calculateAmount;
-    highestOffer = calculateAmount;
-    highestBidder=msg.sender;
-    emit OferringPlaced("offer sent"); // emit event
-  }
-
- //////////////////////
- //////Close offering
- /////////////////////
-     function closeOffering(uint256 p_nftID) public securityFrontRunning(p_nftID){
-        require(s_salesA[p_nftID].owner == msg.sender);
-        s_salesA[p_nftID].status = StateOfAuction.Inactive;
-
-
-              emit auctionClosed("auction closed");                                
-     }  
-    //events 
-
-    event OperatorChanged(
-      string newSate
-   );
-
-    event auctionClosed(
-      string
-    );
-
-    event balanceWithdrawn(
-      string balanceInApp
-    );
-    event OferringPlaced(
-      string offer
-    );
-            
+    event End(address Winner, uint256 BestOffer);
 }
