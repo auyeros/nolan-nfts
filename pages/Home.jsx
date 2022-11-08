fbbf43e3cd9d242b03f0ddfd66b17696af468b02
tomi204 <tomi204@outlook.com>
2022-11-07 22:22:30 -0300

    avanzando con la landing

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,23 @@ const Home = () => {
   return (
     <div className={styles.homeDiv}>
       <div className={styles.navbar}>
-        
+        <ul>
+          <li>
+            <a href="">Social</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.main}>
+        <h1>verita</h1>
+        <div className={styles.submain}>
+          <h2>
+            Building unique solutions to improve the history of ownership and
+            redefine the secondary market for luxury items
+          </h2>
+        </div>
       </div>
     </div>
   );
